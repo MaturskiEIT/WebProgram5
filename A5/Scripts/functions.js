a5fc7d269a5573db4f2893ffb173701d5be80a45
MaturskiEIT <39279514+MaturskiEIT@users.noreply.github.com>
2018-06-04 18:50:10 +0200

    Add files via upload

---
+++ A5/Scripts/functions.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('.sediste').click(function () {
+        $('#broj').val($(this).attr("title"));
+    });
+    $('#slanje').click(function () {
+        var broj = $('#broj').val();
+        if (broj == "") {
+            alert("Niste uneli broj sedista!!!");
+        }
+        else {
+            if (document.getElementById('sediste' + broj).getAttribute('src') == "slike/bus_red.png")
+                alert('Sediste broj ' + broj + ' je zauzeto!');
+            else
+                $('#sediste' + broj).attr('src', 'slike/bus_red.png');
+        }
+    });
+});
